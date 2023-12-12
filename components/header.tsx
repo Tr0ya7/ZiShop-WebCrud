7984f4cbc7f6edf9fd693659bf9e1e04d7ed1180
Luiz Henrique <136819845+Tr0ya7@users.noreply.github.com>
2023-12-12 14:46:58 -0300

    Delete components directory

--- components/header.tsx
+++
@@ -1,9 +0,0 @@
-import { IoAdd } from "react-icons/io5"
-
-export default function Header() {
-    return (
-        <header className="py-5 flex justify-end">
-            <IoAdd className="w-5 cursor-pointer" color="#9fafc5" />
-        </header>
-    )
-}
