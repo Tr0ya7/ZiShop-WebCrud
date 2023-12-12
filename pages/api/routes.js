767f87047ea013336c1b1036ed099eadf9b82dc3
Luiz Henrique <136819845+Tr0ya7@users.noreply.github.com>
2023-12-12 14:47:30 -0300

    Delete pages directory

--- pages/api/routes.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express')
-const { getProducts, postProduct, deleteProduct } = require('./controller')
-const router = Router()
-
-router.get('/', getProducts)
-router.post('/', postProduct)
-router.delete('/', deleteProduct)
-
-module.exports = router
